adcec33628b5f6d517f8a4abfd86c6fef62e4dc4
won <wjin0352@gmail.com>
2015-11-19 20:39:43 -0500

    finished recent_videos instance method for category videos

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -20,5 +20,49 @@ describe Category do
 
   it { should have_many :videos }
 
+  describe "#recent_videos" do
+
+    it 'returns the videos in reverse chronological order by created_at' do
+      comedy = Category.create(name: 'comedy')
+      south_park = Video.create(title: 'south park', description: 'Cartoon comedy about friends', cover_image_url: '/tmp/south_park.jpg', category: comedy, created_at: 1.day.ago)
+      simpsons = Video.create(title: 'simpsons', description: 'show homer and family', cover_image_url: '/tmp/futurama.jpg', category: comedy)
+
+      expect(Category.find_by(name: 'comedy').recent_videos).to eq([simpsons, south_park])
+    end
+
+    it 'returns most recent 6 videos from category' do
+      comedy = Category.create(name: 'comedy')
+
+      6.times { Video.create(title: 'futurama', description: 'space comedy', cover_image_url: '/tmp/futurama.jpg', category: comedy) }
+      simpsons = Video.create(title: 'Simpsons', description: 'homer comedy', cover_image_url: '/tmp/futurama.jpg', category: comedy, created_at: 1.day.ago)
+
+      expect(Category.find_by(:name => 'comedy').recent_videos).not_to include([simpsons])
+    end
+
+    it 'returns all videos in category if there are less than 6 videos' do
+      comedy = Category.create(name: 'comedy')
+
+      4.times { Video.create(title: 'futurama', description: 'space comedy', cover_image_url: '/tmp/futurama.jpg', category: comedy) }
+
+      expect(Category.find_by(name: 'comedy').recent_videos.size).to eq(4)
+    end
+
+    it 'returns first 6 videos in category if there are more than 6 videos in category' do
+      comedy = Category.create(name: 'comedy')
+
+      6.times { Video.create(title: 'futurama', description: 'space comedy', cover_image_url: '/tmp/futurama.jpg', category: comedy) }
+      simpsons = Video.create(title: 'Simpsons', description: 'homer comedy', cover_image_url: '/tmp/futurama.jpg', category: comedy, created_at: 1.day.ago)
+
+
+      expect(Category.find_by(name: 'comedy').recent_videos.size).to eq(6)
+    end
+
+    it 'returns an empty array if category has no videos' do
+      comedy = Category.create(name: 'comedy')
+
+      expect(comedy.videos).to eq([])
+    end
+
+  end
 
 end
